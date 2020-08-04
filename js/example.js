279e6f689fd381a8b7fb198bcc66c51218cce082
RoryWirch <33770280+RoryWirch@users.noreply.github.com>
2020-08-03 21:03:38 -0400

    added list number to heading

--- js/example.js
+++ js/example.js
@@ -21,9 +21,15 @@ ulElement.insertBefore(kaleNode, ulElement.childNodes[0]);
 
 // ADD A CLASS OF COOL TO ALL LIST ITEMS
 var nodeList = ulElement.childNodes;
-
+// update all classes for list elements.
 for (var i = 0; i < nodeList.length; i++){
     nodeList[i].className = "cool";
 }
 
 // ADD NUMBER OF ITEMS IN THE LIST TO THE HEADING
+// get the number of items in the list.
+listNum = document.getElementsByTagName("li").length;
+// get the h2 element
+var header = document.getElementsByTagName("h2");
+// add <span> with dynamic list number.
+header[0].innerHTML += "<span>" + listNum + "</span>";
